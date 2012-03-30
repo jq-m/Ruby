952636a765a812ef38e4aa285e4182fbc76f2c08
Justin Martin <justinqmartin@gmail.com>
2012-03-30 15:37:37 -0400

    added new file

---
+++ hello/hello.rb
@@ -0,0 +1,8 @@
+# Done as part of the "Gif Immersion" labs
+name = ARGV.first || "World"
+
+puts "Hello #{ARGV.first}!"
+
+#do the || operate as an "unless" type argument? Is argument the right word?
+
+#Default is "World"
